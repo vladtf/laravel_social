55b1c68bdd165c66e40f68e5148182d4e1b21897
vladtf <vendeta0263@gmail.com>
2020-04-14 02:52:39 +0300

    Edit profile image

--- app/User.php
+++ app/User.php
@@ -37,11 +37,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function posts(){
-        return $this->hasMany(Post::class)->orderBy('created_at','DESC');
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(
+            function ($user) {
+                $user->profile()->create([
+                   'title' => $user->username,
+                ]);
+            }
+        );
     }
 
-    public function profile(){
+    public function posts()
+    {
+        return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
+    }
+
+    public function profile()
+    {
         return $this->hasOne(Profile::class);
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,7 +5,7 @@
         <div class="pt-5 row">
             <div class="col-md-4 p-5" style="margin: 1px auto;">
                 <img
-                    src="https://instagram.fkiv1-1.fna.fbcdn.net/v/t51.2885-15/e35/s150x150/91714430_341255496835016_6621251509665063906_n.jpg?_nc_ht=instagram.fkiv1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=LhUb5an93MIAX-rsVS1&oh=240df0d1c72b309b1b172f8248bf3229&oe=5EBE161B"
+                    src="/storage/{{ $user->profile->image }}"
                     class="rounded-circle p-3 w-100" alt="">
             </div>
             <div class="col-md-8 pt-5">
