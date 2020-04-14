033b357683ee27aec5989dcbb5d3bfb8b365aab3
vladtf <vendeta0263@gmail.com>
2020-04-14 03:10:50 +0300

    Updated post show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{$post->image}}" class="w-100">
-        </div>
-        <div class="col-4">
-            <h3>{{ $post->user->name }}</h3>
-            <p>{{ $post->caption }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <img src="/storage/{{$post->image}}" class="w-100">
+            </div>
+            <div class="col-4">
+                <div class="d-flex align-items-center">
+                    <div class="pr-2">
+                        <img src="/storage/{{ $post->user->profile->image }}" class="w-100 rounded-circle"
+                             style="max-width: 100px;">
+                    </div>
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}">
+                                <span class="text-dark">{{ $post->user->username }}</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <hr/>
+
+                <p><span class="font-weight-bold">
+                        <a class="pr-2" href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a>
+                    </span>{{ $post->caption }}
+                </p>
+            </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-4 p-5" style="margin: 1px auto;">
                 <img
                     src="/storage/{{ $user->profile->image }}"
-                    class="rounded-circle p-3 w-100" alt="">
+                    class="rounded-circle w-100" alt="">
             </div>
             <div class="col-md-8 pt-5">
                 <div class="d-flex justify-content-between align-items-baseline">
