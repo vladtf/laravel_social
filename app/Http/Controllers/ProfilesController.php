1b3a9285f2258a69257f3a712044aeee2d1f3f81
vladtf <vendeta0263@gmail.com>
2020-04-14 13:42:07 +0300

    Add caching followers, posts and following count

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -12,7 +13,26 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
-        return view('profiles.index', compact('user', 'follows'));
+        $postsCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSecond(30), function () use ($user) {
+            return $user->posts->count();
+        });
+
+        $followersCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSecond(30), function () use ($user) {
+            return $user->profile->followers->count();
+        });
+
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSecond(30), function () use ($user) {
+            return $user->following->count();
+        });
+
+
+        return view('profiles.index', compact('user', 'follows', 'postsCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
@@ -36,7 +56,7 @@ class ProfilesController extends Controller
         if (request('image')) {
             $imagePath = request('image')->store('profile', 'public');
 
-            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
 
             $imageArray = ['image' => $imagePath];
