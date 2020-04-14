caecf0135664c01a6fe1b6daa0c533f063ffcec2
vladtf <vendeta0263@gmail.com>
2020-04-14 12:47:54 +0300

    Linked follows to ui

--- resources/js/components/follow-button.vue
+++ resources/js/components/follow-button.vue
@@ -30,6 +30,11 @@
                     this.status = !this.status;
 
                     console.log(response.data);
+                })
+                .catch(errors =>{
+                    if(errors.response.status = 401){
+                        window.location = '/login';
+                    }
                 });
             }
         },
