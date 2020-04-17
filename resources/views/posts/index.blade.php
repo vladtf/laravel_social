cbcb588fbae7eeb535f3ff8aa7f44d520507e3f3
vladtf <vendeta0263@gmail.com>
2020-04-17 17:04:13 +0300

    Home page style

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,35 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1 class="text-light">Posts</h1>
-        @if($posts->count() < 1)
-            <a href="/profile" style="color: rgba(27,75,114,0.58)">
-            <h3 >Nothing to see here. Go follow someone.</h3>
+    <div class="container col-6">
+        @if( $posts->count() < 1 )
+            <a href="/profile" class="text-info" style="color: rgba(27,75,114,0.58); font-size: large">
+                Nothing to see here. Go follow someone.
             </a>
         @else
-        @foreach($posts as $post)
-            <div class="row">
-                <div class="col-6 offset-3">
-                    <a href="/profile/{{ $post->user->id }}">
-                        <img src="/storage/{{$post->image}}" class="w-100">
-                    </a>
-                </div>
-            </div>
-            <div class="row pt-2 pb-4">
-                <div class="col-6 offset-3">
-                    <div>
-                        <p/>
-                        <p><span class="font-weight-bold">
-                        <a class="pr-2" href="/profile/{{ $post->user->id }}">
-                            <span class="text-dark">{{ $post->user->username }}</span>
+            @foreach($posts as $post)
+                <div class="container post-style p-4">
+                    <div class="row pb-1">
+                        <a class="" href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark"><strong>{{ $post->user->username }}</strong></span>
                         </a>
+                    </div>
+                    <div class="row">
+                        <a href="/p/{{$post->id}}">
+                            <img src="/storage/{{$post->image}}" class="w-100">
+                        </a>
+                    </div>
+                    <div class="row pt-2 pb-4">
+                        <div>
+                            <p/>
+                            <p><span class="font-weight-bold">
                     </span>{{ $post->caption }}
-                        </p>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
         @endif
         <div class="row">
             <div class="col-12 d-flex justify-content-center">
