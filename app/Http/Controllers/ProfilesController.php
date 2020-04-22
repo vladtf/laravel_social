092dff52523f530c4343fcbc24ef339030a3d352
vladtf <vendeta0263@gmail.com>
2020-04-22 03:06:30 +0300

    Add following method to display all the followed profiles

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -19,6 +20,29 @@ class ProfilesController extends Controller
         } else {
             $users = User::paginate(10);
         }
+        return view('profiles.index', [
+            'users' => $users,
+            'search' => $search ?? ''
+        ]);
+    }
+
+    public function following()
+    {
+        $this->middleware('auth');
+
+        $followings = auth()->user()->following->map(function ($follow){
+            return $follow->user;
+        });
+
+        if (\request()->has('search')) {
+            $search = \request('search');
+            $users = $followings->where('username', 'like', "%$search%")
+                ->appends('filter', $search);
+        } else {
+            $users = $followings;
+        }
+
+        $users = $this->paginate($users,10);
 
         return view('profiles.index', [
             'users' => $users,
@@ -75,5 +99,13 @@ class ProfilesController extends Controller
         return redirect("/profile/{$user->id}");
     }
 
+    public function paginate($items, $perPage = 15, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
 
 }
