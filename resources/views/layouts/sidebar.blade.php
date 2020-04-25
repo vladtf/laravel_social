be7a8cad4e0d80ca5a4518f19cbde5089f89b806
vladtf <vendeta0263@gmail.com>
2020-04-25 21:27:49 +0300

    Add sidebar icons

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,14 +7,31 @@
     <ul class="list-unstyled components">
         @auth
             <li>
-                <a href="{{route('post.index')}}">Home</a>
+                <a href="{{route('post.index')}}" class="d-flex justify-content-between align-items-center">
+                    Home
+                    <i class="fa fa-home"></i>
+                </a>
             </li>
             <li>
-                <a href="{{route('profile.show',['user'=>auth()->user()])}}">My profile</a>
+                <a href="{{route('profile.show',['user'=>auth()->user()])}}"
+                   class="d-flex justify-content-between align-items-center">
+                    My profile
+                    <i class="fa fa-user"></i>
+                </a>
+            </li>
+            <li>
+                <a href="{{route('profile.show',['user'=>auth()->user()])}}"
+                   class="d-flex justify-content-between align-items-center">
+                    Following
+                    <i class="fa fa-group"></i>
+                </a>
             </li>
         @endauth
         <li>
-            <a href="{{route('profile.index')}}">Search</a>
+            <a href="{{route('profile.index')}}" class="d-flex justify-content-between align-items-center">
+                Search
+                <i class="fa fa-search"></i>
+            </a>
         </li>
     </ul>
 </div>
